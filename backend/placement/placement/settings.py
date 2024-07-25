f79e52d9edbb4626980f916189991174900a2958
22it100-Neel <22it100@charusat.edu.in>
2024-07-25 22:28:06 +0530

    .

--- backend/placement/placement/settings.py
+++ backend/placement/placement/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e4b)fxs-6zta7v=ia2jp2342r7!7!w2@8i%c=y!j(7b(%37fe$'
+SECRET_KEY = 'django-insecure-weoln7tb8z8-2%7=*4gx$m=q-1h()6(^grf)_9=ol9i5rknj96'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -82,6 +82,7 @@ DATABASES = {
         'PASSWORD': 'Charusat$2024',
         'HOST': 'localhost',  # or the hostname where your MySQL server is running
         'PORT': '3306',      # or the port on which your MySQL server is listening
+
     }
 }
 
