78cec2cac0f2ca11f99f1de34daf20c2df93cc38
22it100-Neel <22it100@charusat.edu.in>
2024-08-29 19:28:34 +0530

    .

--- backend/placement/company/models.py
+++ backend/placement/company/models.py
@@ -1,22 +1,22 @@
-# from django.db import models
-# from student .models import Student_details
-# from faculty .models import Faculty_details
+from django.db import models
+from student .models import Student_details
+from faculty .models import Faculty_details
 
 
-# class CompanyDetails(models.Model):
-#     company_id = models.AutoField(primary_key=True)
-#     comapny_name = models.CharField(max_length=255)
-#     details = models.TextField()
-#     min_package = models.BigIntegerField()
-#     max_package = models.BigIntegerField()
-#     bond = models.IntegerField(null=True)
-#     comapny_hq_location = models.CharField(max_length=255)
-#     work_locations = models.CharField(max_length=255)
-#     comapny_web = models.CharField(max_length=255)
-#     company_contact = models.IntegerField()
+class CompanyDetails(models.Model):
+    company_id = models.AutoField(primary_key=True)
+    comapny_name = models.CharField(max_length=255)
+    details = models.TextField()
+    min_package = models.BigIntegerField()
+    max_package = models.BigIntegerField()
+    bond = models.IntegerField(null=True)
+    comapny_hq_location = models.CharField(max_length=255)
+    work_locations = models.CharField(max_length=255)
+    comapny_web = models.CharField(max_length=255)
+    company_contact = models.IntegerField()
 
-#     def __str__(self):
-#         return self.comapny_name
+    def __str__(self):
+        return self.comapny_name
 
 # # class CompanyPlacementDrive(models.Model):
 # #     company = models.ForeignKey(CompanyDetails, on_delete=models.CASCADE)  # Relation
@@ -32,3 +32,5 @@
 
 # #     def __str__(self):
 # #         return f"{self.company.comapny_name} - {self.date}"
+
+
--- backend/placement/company/views.py
+++ backend/placement/company/views.py
@@ -1,53 +1,60 @@
 
-# # Create your views here.
-# from rest_framework import viewsets
-# from .models import CompanyDetails
-# from .serializers import CompanyDetailsSerializer
+# Create your views here.
+from rest_framework import viewsets
+from .models import CompanyDetails
+from .serializers import CompanyDetailsSerializer
 
-# # views.py
-# from rest_framework import status
-# from rest_framework.response import Response
-# from rest_framework.views import APIView
-# from .serializers import CompanyDetailsSerializer
-# from .models import CompanyDetails
-
-# # class CompanyDetailsedit(APIView):
+# views.py
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import CompanyDetailsSerializer
+from .models import CompanyDetails
 
 
-# #     def post(self, request):
-# #         serializer = CompanyDetailsSerializer(data=request.data, context={'request': request})
-# #         if serializer.is_valid():
-# #             serializer.save()
-# #             return Response(serializer.data, status=status.HTTP_201_CREATED)
-# #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class CompanyDetailsView(APIView):
+    def get(self, request):
+        company_details = CompanyDetails.objects.all()
+        serializer = CompanyDetailsSerializer(company_details, many=True, context={'request': request})
+        return Response(serializer.data)
 
-# #     def put(self, request, pk):
-# #         try:
-# #             company_details = CompanyDetails.objects.get(pk=pk)
-# #         except CompanyDetails.DoesNotExist:
-# #             return Response({"error": "Company details not found"}, status=status.HTTP_404_NOT_FOUND)
-# #         serializer = CompanyDetailsSerializer(company_details, data=request.data, context={'request': request})
-# #         if serializer.is_valid():
-# #             serializer.save()
-# #             return Response(serializer.data)
-# #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class CompanyDetailsPost(APIView):   
+      def post(self, request):
+        serializer = CompanyDetailsSerializer(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# #     def delete(self, request, pk):
-# #         try:
-# #             company_details = CompanyDetails.objects.get(pk=pk)
-# #         except CompanyDetails.DoesNotExist:
-# #             return Response({"error": "Company details not found"}, status=status.HTTP_404_NOT_FOUND)
-# #         serializer = CompanyDetailsSerializer(company_details, context={'request': request})
-# #         serializer.delete(company_details)
-# #         return Response({"message": "Company details deleted successfully"})
+class CompanyDetailsedit(APIView):
+
+    def get_object(self, pk):
+        try:
+            return CompanyDetails.objects.get(pk=pk)
+        except CompanyDetails.DoesNotExist:
+            return None
+        
+    def patch(self, request, pk):
+        company_details = self.get_object(pk)
+        if company_details is None:
+            return Response({"error": "Company details not found"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = CompanyDetailsSerializer(company_details, data=request.data, partial=True, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        try:
+            company_details = CompanyDetails.objects.get(pk=pk)
+        except CompanyDetails.DoesNotExist:
+            return Response({"error": "Company details not found"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = CompanyDetailsSerializer(company_details, context={'request': request},partial=True)
+        serializer.delete(company_details)
+        return Response({"message": "Company details deleted successfully"})
     
 
-# # # class CompanyPlacementDriveViewSet(viewsets.ModelViewSet):
-# # #     queryset = CompanyPlacementDrive.objects.all()
-# # #     serializer_class = CompanyPlacementDriveSerializer
+# class CompanyPlacementDriveViewSet(viewsets.ModelViewSet):
+#     queryset = CompanyPlacementDrive.objects.all()
+#     serializer_class = CompanyPlacementDriveSerializer
 
-# class CompanyDetailsView(APIView):
-#     def get(self, request):
-#         company_details = CompanyDetails.objects.all()
-#         serializer = CompanyDetailsSerializer(company_details, many=True, context={'request': request})
-#         return Response(serializer.data)
