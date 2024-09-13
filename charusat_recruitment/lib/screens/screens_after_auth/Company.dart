3ae8088b38d9fa7a1810a628b16fedb3675c9676
Darshil-05 <22it092@charusat.edu.in>
2024-09-13 20:06:28 +0530

    switch

---
+++ charusat_recruitment/lib/screens/screens_after_auth/Company.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+
+class CompanyPage extends StatefulWidget {
+  const CompanyPage({super.key});
+
+  @override
+  State<CompanyPage> createState() => _CompanyPageState();
+}
+
+class _CompanyPageState extends State<CompanyPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          
+        ],
+      )
+    );
+  }
+}
