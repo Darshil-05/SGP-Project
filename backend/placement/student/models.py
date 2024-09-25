83fad41417cad54350d73cc1d98a003e8ed02e48
22it100-Neel <22it100@charusat.edu.in>
2024-09-25 19:48:32 +0530

    .

--- backend/placement/student/models.py
+++ backend/placement/student/models.py
@@ -5,8 +5,6 @@ from django.contrib.postgres.fields import ArrayField
 
 
 
-
-# Create your models here.
 class StudentManager(BaseUserManager):
     def create_user(self, email, password=None,**extra_fields):
         if not email:
@@ -51,32 +49,12 @@ class Student_details(models.Model):
     cgpa = models.FloatField(max_length=4)
     passing_year = models.IntegerField()
     domains = models.CharField(max_length=255)
-    experience = ArrayField(models.CharField(max_length=255), blank=True, default=list)
-    city = models.CharField(max_length=50,default="changa")
+    # experience = ArrayField(models.CharField(max_length=255), blank=True, default=list)
+    city = models.CharField(max_length=50,default='Unknown City')
     programming_skill = models.CharField(max_length=50,blank=True)
     tech_skill = models.CharField(max_length=100,blank=True)
-    certification = ArrayField(models.CharField(max_length=255),blank=True,default=list)
+    # certification = ArrayField(models.CharField(max_length=255),blank=True,default=list)
     # city,prog-skill and tech-skill,experince,certification
    
     def __str__(self):
         return self.id_no
-    
- 
-    
-
-
-# class UserProfile(models.Model):
-#     user = models.OneToOneField(Student_details, on_delete=models.CASCADE)
-#     is_student = models.BooleanField(default=False)
-
-# {
-#     "id_no": "22it095",
-#     "last_name": "jeet",
-#     "first_name": "patel",
-#     "birthdate": "2004-11-19",
-#     "institute": "CSPIT",
-#     "department": "IT",
-#     "cgpa": "9.8",
-#     "passing_year": "2026",
-#     "domains": "cyber security"
-# }
--- backend/placement/student/permissions.py
+++
@@ -1,5 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class IsStudent(BasePermission):
-    def has_permission(self, request, view):
-        return request.user and hasattr(request.user, 'is_student') and request.user.is_student
